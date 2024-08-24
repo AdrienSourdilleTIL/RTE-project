46f5dcbb0d0407d087c0ac19e577776442827b4b
AdrienSourdille <adrien.sourdille@theinformationlab.co.uk>
2024-08-24 19:06:05 +0100

    action fix 1

--- data_ingestion.py
+++ data_ingestion.py
@@ -4,16 +4,17 @@ from dotenv import load_dotenv
 from api_call import extract_values_to_dataframe
 
 # Load environment variables
-dotenv_path = r'C:\Users\AdrienSourdille\Documents\GitHub\RTE-project\.venv\Scripts\.env'
+dotenv_path = r'C:\Users\AdrienSourdille\Documents\GitHub\RTE-project\.venv'
 load_dotenv(dotenv_path)
 
 SNOWFLAKE_USER = os.getenv('SNOWFLAKE_USER')
+print(SNOWFLAKE_USER)
 SNOWFLAKE_PASSWORD = os.getenv('SNOWFLAKE_PASSWORD')
 SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
 SNOWFLAKE_WAREHOUSE = os.getenv('SNOWFLAKE_WAREHOUSE')
 SNOWFLAKE_DATABASE = os.getenv('SNOWFLAKE_DATABASE')
 SNOWFLAKE_SCHEMA = os.getenv('SNOWFLAKE_SCHEMA')
-SNOWFLAKE_TABLE = 'ELECTRICITY_CONSUMPTION'
+SNOWFLAKE_TABLE = os.getenv('ELECTRICITY_CONSUMPTION')
 
 def upload_dataframe_to_snowflake(df):
     if df is None:
